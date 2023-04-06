92c00e64852bbf643173cc8842e416efaf0a817c
Gibsol <gibsol75@gmail.com>
2023-04-06 18:55:05 +0300

    :sparkles: Implement user written data when creating a new

--- src/user.rs
+++ src/user.rs
@@ -3,16 +3,35 @@ use reqwest::Error;
 
 #[tokio::main]
 pub async fn create_user() -> Result<(), Error> {
+    let prompts = vec![
+        "Enter your username:",
+        "Enter your first name:",
+        "Enter your last name:",
+        "Enter your new password:",
+    ];
+
+    let mut inputs = vec![String::new(); prompts.len()];
+
+    for (i, prompt) in prompts.iter().enumerate() {
+        println!("{} ", prompt);
+        std::io::stdin().read_line(&mut inputs[i]).unwrap();
+    }
+
+    let username = &inputs[0];
+    let first_name = &inputs[1];
+    let last_name = &inputs[2];
+    let new_password = &inputs[3];
+
     let client = reqwest::Client::new();
 
     let params = [
-        ("username", "John"),
-        ("firstname", "Ivanov"),
-        ("lastname", "Ivanov"),
-        ("newPassword", "Ivanov123"),
+        ("username", &username),
+        ("firstname", &first_name),
+        ("lastname", &last_name),
+        ("newPassword", &new_password),
     ];
     let response = client
-        .post(&format!("{API_URL}/users"))
+        .post(&format!("{}/users", API_URL))
         .form(&params)
         .send()
         .await?;
