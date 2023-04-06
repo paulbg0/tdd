fddf409b31a79a768be57891b19b260f148a4bd5
Gibsol <gibsol75@gmail.com>
2023-04-06 19:33:31 +0300

    :sparkles: Implementation of task

--- src/args.rs
+++ src/args.rs
@@ -2,6 +2,18 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Args {
-    #[arg(short, long)]
+    #[arg(long)]
     pub create_user: bool,
+
+    #[arg(short, long)]
+    pub view_profile: bool,
+
+    #[arg(short, long)]
+    pub create_task: bool,
+
+    #[arg(short, long)]
+    pub get_tasks: bool,
+
+    #[arg(short, long, default_value = "0")]
+    pub delete_task: u32,
 }
