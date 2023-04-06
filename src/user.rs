4d9095b027e1523e024fcbe807a29d3c15aadabc
Gibsol <gibsol75@gmail.com>
2023-04-06 18:51:45 +0300

    :sparkles: Implement basic hardcoded post request

--- src/user.rs
+++ src/user.rs
@@ -1,3 +1,23 @@
-pub fn create_user() {
-    println!("Creating user");
+use crate::consts::API_URL;
+use reqwest::Error;
+
+#[tokio::main]
+pub async fn create_user() -> Result<(), Error> {
+    let client = reqwest::Client::new();
+
+    let params = [
+        ("username", "John"),
+        ("firstname", "Ivanov"),
+        ("lastname", "Ivanov"),
+        ("newPassword", "Ivanov123"),
+    ];
+    let response = client
+        .post(&format!("{API_URL}/users"))
+        .form(&params)
+        .send()
+        .await?;
+
+    println!("{}", response.text().await?);
+
+    Ok(())
 }
