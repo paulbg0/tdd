713be5252c3ab24e2132b26dd4723c039e33ff67
Gibsol <gibsol75@gmail.com>
2023-04-11 20:13:58 +0300

    :sparkles: Remove and add
    Remove create_user() from main (it will be used in some other function).
    Rename view_profile to show_profile.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 use args::Args;
 use clap::Parser;
 use colored::*;
+use logging::logging;
 use task::{create_task, delete_task, get_tasks, update_task, view_task};
-use user::{create_user, view_profile};
+use user::show_profile;
 
 mod args;
 mod consts;
 mod errors;
+mod logging;
 mod task;
 mod user;
 
 fn main() {
+    logging().unwrap();
+
     let args = Args::parse();
 
     match args {
         Args {
-            create_user: true, ..
-        } => create_user().unwrap(),
-        Args {
-            view_profile: true, ..
-        } => view_profile(12).unwrap(),
+            show_profile: true, ..
+        } => show_profile(12).unwrap(),
         Args {
             create_task: true, ..
         } => create_task().unwrap(),
@@ -33,10 +34,6 @@ fn main() {
             update_task: id, ..
         } if id > 0 => update_task(id).expect("Invalid ID"),
         Args { view_task: id, .. } if id > 0 => view_task(id).expect("Invalid ID"),
-        _ => println!(
-            "{}\n{}",
-            "No valid arguments provided.".red().bold(),
-            "Please use --h for more information.".italic()
-        ),
+        _ => println!("{}", "-h for information about the commands".italic()),
     }
 }
